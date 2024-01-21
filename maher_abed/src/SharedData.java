afee270b18f7fd570ecc95902fa2bae6679ad384
abode <abode@10.10.14.232>
2024-01-21 13:08:58 +0200

    last

--- maher_abed/src/SharedData.java
+++ maher_abed/src/SharedData.java
@@ -1,45 +1,75 @@
 import java.util.ArrayList;
+/*
+ * @author abode
+ * @author maher
+ * version 1
+ * */
 
-public class SharedData 
-{
-	private ArrayList<Integer> array;
-	private boolean [] winArray;
-	private boolean flag;
-	private final int b;
-	
-	public SharedData(ArrayList<Integer> array, int b) {
-		
-		this.array = array;
-		this.b = b;
-	}
+/**
+ * Represents shared data used in a program.
+ */
+public class SharedData {
 
-	public boolean[] getWinArray() 
-	{
-		return winArray;
-	}
+    private ArrayList<Integer> array;
+    private boolean[] winArray;
+    private boolean flag;
+    private final int b;
 
-	public void setWinArray(boolean [] winArray) 
-	{
-		this.winArray = winArray;
-	}
+    /**
+     * Constructs a new instance of the SharedData class.
+     * @param array The ArrayList of integers to be shared.
+     * @param b     An integer value to be shared.
+     */
+    public SharedData(ArrayList<Integer> array, int b) {
+        this.array = array;
+        this.b = b;
+    }
 
-	public ArrayList<Integer> getArray() 
-	{
-		return array;
-	}
+    /**
+     * Gets the array of booleans indicating winning status.
+     * @return The array of booleans indicating winning status.
+     */
+    public boolean[] getWinArray() {
+        return winArray;
+    }
 
-	public int getB() 
-	{
-		return b;
-	}
+    /**
+     * Sets the array of booleans indicating winning status.
+     * @param winArray The array of booleans indicating winning status.
+     */
+    public void setWinArray(boolean[] winArray) {
+        this.winArray = winArray;
+    }
 
-	public boolean getFlag() 
-	{
-		return flag;
-	}
+    /**
+     * Gets the ArrayList of integers.
+     * @return The ArrayList of integers.
+     */
+    public ArrayList<Integer> getArray() {
+        return array;
+    }
 
-	public void setFlag(boolean flag) {
-		this.flag = flag;
-	}
+    /**
+     * Gets the integer value.
+     * @return The integer value.
+     */
+    public int getB() {
+        return b;
+    }
 
+    /**
+     * Gets the flag indicating a certain condition.
+     * @return The flag indicating a certain condition.
+     */
+    public boolean getFlag() {
+        return flag;
+    }
+
+    /**
+     * Sets the flag indicating a certain condition.
+     * @param flag The flag indicating a certain condition.
+     */
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
